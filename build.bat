67da4caf24dd083a028af6320870779db3bda633
EnderMinecraft <88361566+EnderMinecraft@users.noreply.github.com>
2022-11-07 21:18:01 +0700

    Update build.bat

--- build.bat
+++ build.bat
@@ -4,6 +4,7 @@ del /q main.spec
 pyinstaller -w --onefile -i ".\icon.ico" .\main.pyw
 md ..\output
 move /y .\dist\main.exe ..\output\
+ren main.exe AmogusClck.exe
 copy /y .\*.gif ..\output
 copy /y .\*.wav ..\output
 copy /y .\*.ico ..\output
@@ -11,11 +12,8 @@ copy /y .\*.ico ..\output
 echo Complete
 echo Would you want to run the output?(Y/N)
 choice /n 
-
 If ERRORLEVEL = 1 goto execute
 If ERRORLEVEL = 0 goto end
-
-
 :execute
 start ..\output\main.exe
 :end
