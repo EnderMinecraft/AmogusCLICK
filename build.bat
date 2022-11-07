593c9073206c6f73e34b55c11a41dd233a39d790
EnderMinecraft <88361566+EnderMinecraft@users.noreply.github.com>
2022-11-07 20:26:07 +0700

    M O R E   A U T O M A T E D

--- build.bat
+++ build.bat
@@ -9,3 +9,14 @@ copy /y .\*.wav ..\output
 copy /y .\*.ico ..\output
 @echo off
 echo Complete
+echo Would you want to run the output?(Y/N)
+choice /n 
+
+If ERRORLEVEL = 1 goto execute
+If ERRORLEVEL = 0 goto end
+
+
+:execute
+start ..\output\main.exe
+:end
+exit
--- insreqlib.bat
+++ insreqlib.bat
@@ -1,13 +1,22 @@
 @echo off
+
 pip install pyinstaller
 
+
 echo Would you want to build the script now?(Y/N)
+
 choice /n 
+
 If ERRORLEVEL = 1 goto build
+
 If ERRORLEVEL = 0 goto end
 
+
 :end
+
 exit
 
+
 :build
+
 call .\build.bat
