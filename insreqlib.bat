a922188b33543210558feb2de3279cd4490e5509
EnderMinecraft <88361566+EnderMinecraft@users.noreply.github.com>
2022-11-07 19:40:30 +0700

    Update insreqlib.bat

--- insreqlib.bat
+++ insreqlib.bat
@@ -1,1 +1,13 @@
+@echo off
 pip install pyinstaller
+
+echo Would you want to build the script now?(Y/N)
+choice /n 
+If ERRORLEVEL = 1 goto build
+If ERRORLEVEL = 0 goto end
+
+:end
+exit
+
+:build
+call .\build.bat
