e9a1056faa26cbceb2b2f291e3c1b39ab27d562e
EnderMinecraft <88361566+EnderMinecraft@users.noreply.github.com>
2023-06-28 21:34:19 +0700

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,13 +1,15 @@
 cd src
 rd /s /q dist
+rd /s /q build
 del /q main.spec
-pyinstaller --onefile -i ".\icon.ico" .\main.py
+pyinstaller --onefile --uac-admin -i ".\icon.ico" .\main.py
 md ..\output
 move /y .\dist\main.exe ..\output\
 ren main.exe AmogusClck.exe
 copy /y .\*.gif ..\output
 copy /y .\*.wav ..\output
 copy /y .\*.ico ..\output
+copy /y .\*.json ..\output
 @echo off
 echo Complete
 echo Would you want to run the output?(Y/N)
